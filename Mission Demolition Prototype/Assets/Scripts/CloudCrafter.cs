2b1677d119e198d1f0ef6cac37d299bf64997f58
Colton Krantz <35778556+krantz27@users.noreply.github.com>
2018-02-16 11:01:23 -0600

    Add clouds
    background clouds added

---
+++ Mission Demolition Prototype/Assets/Scripts/CloudCrafter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudCrafter : MonoBehaviour {
+    [Header("Set in Inspector")]
+    public int numClouds = 40;
+    public GameObject cloudPrefab;
+    public Vector3 cloudPosMin = new Vector3(-50, -5, 10);
+    public Vector3 cloudPosMax = new Vector3(150, 100, 10);
+    public float cloudScaleMin = 1;
+    public float cloudScaleMax = 3;
+    public float cloudSpeedMult = 0.5f;
+    private GameObject[] cloudInstances;
+   
+    void Awake () {
+
+        cloudInstances = new GameObject[numClouds];
+        GameObject anchor = GameObject.Find("CloudAnchor");
+        //Iterate through and make a cloud
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Mission Demolition Prototype/Assets/Scripts/FollowCam.cs
+++ Mission Demolition Prototype/Assets/Scripts/FollowCam.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class FollowCam : MonoBehaviour {
     public static GameObject POI;
-    
+
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
+
     [Header("Set Dynamically")]
     public float camZ;
     // Use this for initialization
@@ -20,9 +25,17 @@ public class FollowCam : MonoBehaviour {
             return;
         }
         Vector3 destination = POI.transform.position;
+
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+
+        destination = Vector3.Lerp(transform.position, destination, easing);
+
         destination.z = camZ;
 
         transform.position = destination;
+
+        Camera.main.orthographicSize = destination.y + 10;
 
     }
 }
